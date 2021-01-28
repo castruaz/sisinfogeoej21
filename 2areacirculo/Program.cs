5c95011316c03533f43f50adfba4e4eddd2b8ae9
Carlos Castaneda <castr@MacBook-Pro-de-Carlos.local>
2021-01-28 11:29:55 -0600

    cambios

---
+++ 2areacirculo/Program.cs
@@ -0,0 +1,24 @@
+﻿// Programa que calcula el area de un circulo
+using System;
+
+namespace _2areacirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio, area;
+
+            Console.WriteLine("Calculando el area de un circulo\n");
+            Console.WriteLine("Dame el Radio");
+
+            radio=float.Parse(Console.ReadLine());
+
+            area = Math.PI * Math.Pow(radio,2);
+
+            Console.WriteLine("El area del circulo es {0}", area);
+
+
+        }
+    }
+}
---
+++ 4pagatrabajador/Program.cs
@@ -0,0 +1,39 @@
+﻿// Paga del trabajdor
+
+using System;
+
+namespace _4pagatrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int horas;
+            float paga, tasa=0.10F;
+
+            float impuesto, pagabruta, paganeta;
+
+            Console.WriteLine("Calculo de la paga de un trabajador \n");
+
+            Console.Write("Nombre del trabajador: "); nombre = Console.ReadLine();
+            Console.Write("Horas trabajadas     : "); horas = int.Parse(Console.ReadLine());
+            Console.Write("Paga por hora        : "); paga = float.Parse(Console.ReadLine());
+
+            pagabruta = horas * paga;
+            impuesto = pagabruta * tasa;
+            paganeta = pagabruta - impuesto;
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Estimado " + nombre);
+            Console.WriteLine($"Trabajaste {horas} horas, con una paga por hora de {paga} \n");
+            Console.WriteLine("Paga Bruta   : {0}", pagabruta);
+            Console.WriteLine("Impuesto     : {0}", impuesto);
+            Console.WriteLine("Paga Neta    : {0}", paganeta);
+            Console.WriteLine("Trabajaste {0} horas, con una paga por hora de {1} \n",horas,paga);
+
+
+            
+        }
+    }
+}
