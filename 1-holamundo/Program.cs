0f33fd457d14e7bf15438f1683c3c324c699424c
castruaz <castr@uaz.edu.mx>
2021-01-27 18:59:23 -0600

    hola

---
+++ 1-holamundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+             Console.WriteLine("Excelente");
+            Console.WriteLine("Excelente");
+        }
+    }
+}
