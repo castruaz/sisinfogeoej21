a17da29b1f6a604d472c6787f0fa99f03859dbb0
castruaz <castr@uaz.edu.mx>
2021-04-20 11:44:24 -0500

    va

---
+++ 42webscraping3_p/Program.cs
@@ -0,0 +1,32 @@
+﻿// https://stackoverflow.com/questions/18427257/htmlagilitypack-loading-multiple-pages
+
+using System;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+
+namespace _42webscraping3_p
+{
+    class Pelicula {
+        string Titulo {get; set;}
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = @"https://www.imdb.com/chart/top/";
+             List <Pelicula> datosPelicula = new List<Pelicula>();
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = new HtmlDocument();
+            
+
+
+            Console.WriteLine("Lista de peliculas en el tpo de www.imbd.com");
+
+
+
+        }
+    }
+}
